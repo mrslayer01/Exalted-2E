aef7c8ef46ed8ea0afb9b1f4e9e54add5dec8b6e
lyle <lyle_barber@yahoo.com>
2023-11-05 01:00:27 -0400

    replaced all mentions of boilerplate

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,23 @@
-export const BOILERPLATE = {};
+export const EXALTED2E = {};
 
 /**
  * The set of Ability Scores used within the sytem.
  * @type {Object}
  */
- BOILERPLATE.abilities = {
-  "str": "BOILERPLATE.AbilityStr",
-  "dex": "BOILERPLATE.AbilityDex",
-  "con": "BOILERPLATE.AbilityCon",
-  "int": "BOILERPLATE.AbilityInt",
-  "wis": "BOILERPLATE.AbilityWis",
-  "cha": "BOILERPLATE.AbilityCha"
+EXALTED2E.abilities = {
+    "str": "EXALTED2E.AbilityStr",
+    "dex": "EXALTED2E.AbilityDex",
+    "con": "EXALTED2E.AbilityCon",
+    "int": "EXALTED2E.AbilityInt",
+    "wis": "EXALTED2E.AbilityWis",
+    "cha": "EXALTED2E.AbilityCha"
 };
 
-BOILERPLATE.abilityAbbreviations = {
-  "str": "BOILERPLATE.AbilityStrAbbr",
-  "dex": "BOILERPLATE.AbilityDexAbbr",
-  "con": "BOILERPLATE.AbilityConAbbr",
-  "int": "BOILERPLATE.AbilityIntAbbr",
-  "wis": "BOILERPLATE.AbilityWisAbbr",
-  "cha": "BOILERPLATE.AbilityChaAbbr"
+EXALTED2E.abilityAbbreviations = {
+    "str": "EXALTED2E.AbilityStrAbbr",
+    "dex": "EXALTED2E.AbilityDexAbbr",
+    "con": "EXALTED2E.AbilityConAbbr",
+    "int": "EXALTED2E.AbilityIntAbbr",
+    "wis": "EXALTED2E.AbilityWisAbbr",
+    "cha": "EXALTED2E.AbilityChaAbbr"
 };

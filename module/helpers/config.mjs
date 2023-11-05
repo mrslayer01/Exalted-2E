45e9b567e4a77f11bdf06d321ea4a79b244fd838
lyle <lyle_barber@yahoo.com>
2023-11-05 01:03:47 -0500

    Fixed attrib

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -8,22 +8,22 @@ EXALTED2E.abilities = {
     "str": "EXALTED2E.AbilityStr",
     "dex": "EXALTED2E.AbilityDex",
     "stm": "EXALTED2E.AbilityStm",
+    "cha": "EXALTED2E.AbilityCha",
     "man": "EXALTED2E.AbilityMan",
-    "wis": "EXALTED2E.AbilityWis",
     "apr": "EXALTED2E.AbilityApr",
     "per": "EXALTED2E.AbilityPer",
     "int": "EXALTED2E.AbilityInt",
-    "wit": "EXALTED2E.AbilityWit"
+    "wit": "EXALTED2E.AbilityWit",
 };
 
 EXALTED2E.abilityAbbreviations = {
     "str": "EXALTED2E.AbilityStrAbbr",
     "dex": "EXALTED2E.AbilityDexAbbr",
     "stm": "EXALTED2E.AbilityStmAbbr",
+    "cha": "EXALTED2E.AbilityChaAbbr",
     "man": "EXALTED2E.AbilityManAbbr",
-    "wis": "EXALTED2E.AbilityWisAbbr",
     "apr": "EXALTED2E.AbilityAprAbbr",
     "per": "EXALTED2E.AbilityPerAbbr",
     "int": "EXALTED2E.AbilityIntAbbr",
-    "wit": "EXALTED2E.AbilityWitAbbr"
+    "wit": "EXALTED2E.AbilityWitAbbr",
 };

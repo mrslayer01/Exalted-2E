4d2cc4e649ae0a68bc34a4dc3e9ebd72a9fe2cd2
lyle <lyle_barber@yahoo.com>
2023-11-08 22:31:20 -0500

    replaced with newer version
    ss

---
+++ e2e/module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const E2E = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ E2E.abilities = {
+  "str": "E2E.AbilityStr",
+  "dex": "E2E.AbilityDex",
+  "con": "E2E.AbilityCon",
+  "int": "E2E.AbilityInt",
+  "wis": "E2E.AbilityWis",
+  "cha": "E2E.AbilityCha"
+};
+
+E2E.abilityAbbreviations = {
+  "str": "E2E.AbilityStrAbbr",
+  "dex": "E2E.AbilityDexAbbr",
+  "con": "E2E.AbilityConAbbr",
+  "int": "E2E.AbilityIntAbbr",
+  "wis": "E2E.AbilityWisAbbr",
+  "cha": "E2E.AbilityChaAbbr"
+};
